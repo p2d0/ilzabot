7a18de898e324abd8a1e3cb8882a1c13dbb25326
Patriot720 <cerkin-3@yandex.ru>
2016-05-20 15:13:49 +0300

    re

--- iLzabot.py
+++ iLzabot.py
@@ -1,16 +1,16 @@
+# -*- coding: utf8 -*-
 import requests
 import time
-from responses import responses_list
+import responses
 import random
-
 random.seed(53231)
+def find_msg(num):
+	return get_update['result'][len(get_update['result'])-num]['message']['text'].lower().replace('?','').replace('!','')
 def send_msg(msg):
 	random.seed(time.time())
 	text = last_message['text']
 	chat_id = get_update['result'][len(get_update['result'])-1]['message']['chat']['id']
 	send_msg = requests.post(get_info('sendMessage'),{'chat_id': chat_id, 'text': msg, 'reply_to_message_id': last_message['message_id']})
-def get_updates():
-	return requests.get(get_info('getUpdates')).json()
 def get_info(method_name):
 	return 'https://api.telegram.org/bot233787808:AAH71m_JtqkQP5ZADD2yxYI2ye8TKTeMnxE/' + method_name
 def post_msg():
@@ -18,22 +18,41 @@ def post_msg():
 	text = last_message['text']
 	data = {}
 	print(chat_id)
-	if 'нет' in text[len(text)-3:]:
+	if 'нет' in text[len(text)-3:].lower():
 		print (text[len(text)-3:])
-		send_msg(responses_list[random.randint(0,len(responses_list))])
+		send_msg(responses.responses_list[random.randint(0,len(responses.responses_list))])
 	elif 'https://osu.ppy.sh/ss/' in text:
 		send_msg('Ебнутый')
-		
+	elif find_msg(1) in responses.pizda_list and (find_msg(2) in responses.pizda_list or find_msg(3) in responses.pizda_list): 
+		send_msg(' '.join(responses.pizda_list[2:]))  
+	elif find_msg(1) in responses.kakay_list[0][0]:
+		send_msg(' '.join(responses.kakay_list[0]))
+	elif find_msg(1) in responses.ruka_list:
+		send_msg(' '.join(responses.ruka_list))
+	elif find_msg(1) in responses.money_list:
+		send_msg(' '.join(responses.money_list))
+	elif 'иди нахуй' == find_msg(1):
+		send_msg(responses.hui)
+	elif 'можно' == find_msg(1):
+		send_msg('можно машку за ляжку и козу на возу')
+	elif 'интересно ' in find_msg(1):
+		send_msg('интересно когда в бане тесно') 
+	elif find_msg(1) in responses.zhui_list and responses.zhui_list.index(find_msg(1)) != responses.zhui_list[-1]:
+		send_msg(responses.zhui_list[responses.zhui_list.index(find_msg(1))+1])
+	elif 'кто пидор' in find_msg(1):
+		send_msg('Серега')
+
 id = 0
 get_update = requests.get(get_info('getUpdates')).json()
 last_message = get_update['result'][len(get_update['result'])-1]['message']
 while True:
 	if last_message['message_id'] == id:
-		get_update = requests.get(get_info('getUpdates')).json()
+		get_update = requests.get(get_info('getUpdates'),{'offset':get_update['result'][len(get_update['result'])-1]['update_id']-2}).json()
 		last_message = get_update['result'][len(get_update['result'])-1]['message']
 		time.sleep(0.5)
 		continue
 	id = last_message['message_id']
 	print(last_message['text'])
+	print(get_update['result'][len(get_update['result'])-1]['update_id'])
 	post_msg()
 	
