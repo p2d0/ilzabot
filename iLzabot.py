f2e0bcdeed38648ba05761aa05671beabadf2398
patriot720 <cerkin-3@yandex.ru>
2023-10-24 12:25:54 +0300

    fix

--- iLzabot.py
+++ iLzabot.py
@@ -137,7 +137,6 @@ async def handle_chatbot(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     if "нет" in response_text.lower():
         await gigachad_vid(f"@{update.message.from_user.username}\: {update.message.text}",f"@iLza_bot\: {text}")
         await update.message.reply_video("./output_final.mp4")
-    await update.message.reply_text(text)
 
 
 async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
